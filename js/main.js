855a7ae0d7adc398c0e4273e8401e10901f5335c
Kaja Piotrowska <kaja.piotrowska@creadis.com>
2019-01-03 16:03:01 +0100

    css style & bootstrap fixed

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+var controller = new ScrollMagic.Controller();
+
+var ourScene = new ScrollMagic.Scene({
+  triggerElement: 'project01'
+})
+
+.setClassToggle('#project01','fade-in')
+.addTo(controller);
+});
